057ada5269e161936bb598814b6f6d5bc0555913
rich smith <richloveswork@gmail.com>
2013-01-25 15:13:04 +1100

    latest

---
+++ ArtSite/Filters/EmailAddressAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ArtSite.Filters
+{
+    public class EmailAddressAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var address = (string)value;
+            if (string.IsNullOrEmpty(address))
+            {
+                return true;
+            }
+            var objNotNaturalPattern = new Regex(@"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b");
+
+            return objNotNaturalPattern.IsMatch(address);
+
+        }
+    }
+}
--- ArtSite/Models/AccountModels.cs
+++ ArtSite/Models/AccountModels.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Web.Mvc;
-using System.Web.Security;
 
 namespace ArtSite.Models
 {
